33f5e0012a9164ae11942b95f678d5bb0399e4f1
ZahraBaig166 <zahrabaig166@gmail.com>
2024-05-18 19:49:21 +0500

    delete,edit modified

--- express/models/Products.js
+++ express/models/Products.js
@@ -8,3 +8,20 @@ const productSchema = new mongoose.Schema({
 });
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
+
+
+// const mongoose = require("mongoose");
+
+// const productSchema = new mongoose.Schema({
+//   id: Number,
+//   name: String,
+//   category: String,
+//   type: String,
+//   price: Number,
+//   description: String,
+//   image: Buffer
+// });
+
+// const Product = mongoose.model("Product", productSchema);
+
+// module.exports = Product;
--- express/routes/products.js
+++ express/routes/products.js
@@ -3,28 +3,28 @@ const req = require("express/lib/request");
 const router = express.Router();
 const Product = require("../models/Products");
 
-router.get("/products/new", async (req, res) => {
-  res.render("products/new");
+router.get("/new", async (req, res) => {
+  res.render("new",{pageContent:"new"});
 });
 
 router.post("/products/new", async (req, res) => {
   let record = new Product(req.body);
   await record.save();
-  return res.redirect("/products");
+  return res.redirect("/list");
 });
 
-router.get("/products/:id/delete", async (req, res) => {
+router.get("/:id/delete", async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   return res.redirect("/list");  
 });
 
 
-router.get("/products/:id/edit", async (req, res) => {
+router.get("/:id/edit", async (req, res) => {
   let product = await Product.findById(req.params.id);
-  res.render("edit", { product });  
+  res.render("edit", { pageContent:"edit",product });  
 });
 
-router.post("/products/:id/edit", async (req, res) => {
+router.post("/:id/edit", async (req, res) => {
   let product = await Product.findById(req.params.id);
   product.name = req.body.name;
   product.category = req.body.category;
